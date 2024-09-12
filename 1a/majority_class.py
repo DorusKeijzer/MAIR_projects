2a922fa712a268a566632fcdf51634c27e8ca8e2
Dorus <doruskeijzer@gmail.com>
2024-09-12 11:46:16 +0200

    majority model

---
+++ 1a/majority_class.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+class majority_class_model:
+    def __init__(self, labels):
+        counter = Counter()
+        for label in labels:
+            counter[label] += 1
+        self.majority_class = counter.most_common(1)[0]
+
+    def predict(self, sentence: str):
+        return self.majority_class
+
+
+if __name__ == "__main__":
+    from read_data import train_label
+    mcm = majority_class_model(train_label)
+    print(mcm.predict("test sentence"))
