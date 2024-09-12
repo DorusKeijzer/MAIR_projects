344bb206347121ed275c76afdd9b2382d0eb0659
Dorus <doruskeijzer@gmail.com>
2024-09-12 11:02:23 +0200

    addded bag of words to dataset

--- 1a/read_data.py
+++ 1a/read_data.py
@@ -1,15 +1,25 @@
 import os
+from sklearn.feature_extraction.text import CountVectorizer
 
-data_path = os.path.join("data", "dialog_acts.dat")
+data_path = os.path.join("../data", "dialog_acts.dat")
 
-dataset = []
+labels = []
+sentences = []
 
 with open(data_path) as file:
     for line in file:
         line = line.split()
-        label = line[0]
-        sentence = line[1:]
-        dataset.append((label, sentence))
+        labels.append(line[0])
 
-print(dataset[0:10])
-print(len(dataset))
+        sentence = " ".join(line[1:])
+        sentences.append(sentence)
+
+vectorizer = CountVectorizer()
+vectorizer.fit(sentences)
+
+bag_of_words = vectorizer.transform(sentences)
+
+dataset = list(zip(labels, bag_of_words))
+
+if __name__ == "__main__":
+    print(dataset)
