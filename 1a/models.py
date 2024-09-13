70c6b284028843749231da19071794e4774534b4
Dorus <doruskeijzer@gmail.com>
2024-09-13 08:50:22 +0200

    moved all models into models.py

--- 1a/models.py
+++ 1a/models.py
@@ -1,5 +1,18 @@
+from abc import ABC, abstractmethod
 from collections import Counter
-from base_model import Model
+
+
+class Model(ABC):
+    """All models inherit from this class so we have a uniform way of calling them. Models implement __init__ and a predict function"""
+
+    @abstractmethod
+    def __init__(self, data: list, labels: list):
+        self.name = "Model"
+        pass
+
+    @abstractmethod
+    def predict(self, sentence: str) -> str:
+        pass
 
 
 class MajorityClassModel(Model):
