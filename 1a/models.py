b05a0e24523fceb0f6b631ab00d0b2697030e2e3
Dorus <doruskeijzer@gmail.com>
2024-09-16 14:30:19 +0200

    refactoring models

--- 1a/models.py
+++ 1a/models.py
@@ -12,6 +12,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
 import numpy as np
+from joblib import dump
+from sklearn.base import BaseEstimator
+
 
 class Model(ABC):
     """All models inherit from this class so we have a uniform way of calling them. Models implement __init__ and a predict function."""
@@ -19,15 +22,17 @@ class Model(ABC):
     @abstractmethod
     def __init__(self, data: list, labels: list):
         self.name = "Model"
-        pass
+        self.data = data
+        self.labels = labels
 
     @abstractmethod
     def predict(self, sentence: str) -> str:
         pass
 
+
 class RuleBasedModel(Model):
     """Classifies sentences based on keyword matching for each dialog act."""
-    
+
     def __init__(self, data, labels):
         super().__init__(data, labels)
         self.name = "Rule-based model"
@@ -50,14 +55,15 @@ class RuleBasedModel(Model):
         }
 
     def predict(self, sentence: str) -> str:
-        """Predict the dialog act based on the presence of keywords, ensuring word boundaries."""       
+        """Predict the dialog act based on the presence of keywords, ensuring word boundaries."""
         # Check each dialog act with word boundaries
         for dialog_act, keywords in self.rules.items():
             for keyword in keywords:
                 if re.search(r'\b' + re.escape(keyword) + r'\b', sentence):
                     return dialog_act
-        
+
         return "inform"  # Fallback to "inform" if no keywords match
+
 
 class MajorityClassModel(Model):
     """Calculates the majority class upon initialization and always predicts this class when predicting."""
@@ -71,35 +77,45 @@ class MajorityClassModel(Model):
     def predict(self, sentence: str):
         return self.majority_class
 
-class DecisionTreeModel(Model):
-    """Decision Tree classifier based on bag-of-words features."""
 
-    def __init__(self, data, labels):
+class ScikitModel(Model):
+    """Base class for models using scikit-learn classifiers."""
+
+    def __init__(self, data, labels, model: BaseEstimator, name: str):
         super().__init__(data, labels)
-        self.name = "Decision Tree model"
-        self.model = DecisionTreeClassifier()
-        self.model.fit(data, labels)
+        self.name = name
+        self.model = model
+
+    def train(self):
+        self.model.fit(self.data, self.labels)
+
+    def store(self, path=None):
+        if path is None:
+            path = f"model_weights_{
+                self.name.lower().replace(' ', '_')}.joblib"
+        dump(self.model, path)
 
     def predict(self, sentence: str) -> str:
         # Convert the raw sentence to a bag-of-words feature vector
         sentence_bow = vectorizer.transform([sentence])
-        # Predict the label using the decision tree model
+        # Predict the label using the model
         return self.model.predict(sentence_bow)[0]
 
-class LogisticRegressionModel(Model):
+
+class DecisionTreeModel(ScikitModel):
+    """Decision Tree classifier based on bag-of-words features."""
+
+    def __init__(self, data, labels):
+        super().__init__(data, labels, model=DecisionTreeClassifier(), name="Decision Tree model")
+
+
+class LogisticRegressionModel(ScikitModel):
     """Logistic Regression classifier based on bag-of-words features."""
 
     def __init__(self, data, labels):
-        super().__init__(data, labels)
-        self.name = "Logistic Regression model"
-        self.model = LogisticRegression(max_iter=200)
-        self.model.fit(data, labels)
+        super().__init__(data, labels, model=LogisticRegression(
+            max_iter=200), name="Logistic Regression model")
 
-    def predict(self, sentence: str):
-        # Convert the raw sentence to a bag-of-words feature vector
-        sentence_bow = vectorizer.transform([sentence])
-        # Predict the label using the logistic regression model
-        return self.model.predict(sentence_bow)[0]
 
 class FeedForwardNNModel(Model):
     """Feed Forward Neural Network classifier based on bag-of-words features using Keras."""
@@ -107,16 +123,25 @@ class FeedForwardNNModel(Model):
     def __init__(self, data, labels):
         super().__init__(data, labels)
         self.name = "Feed Forward NN model"
+
         self.num_classes = len(set(labels))
         self.model = Sequential([
             Dense(100, activation='relu', input_shape=(data.shape[1],)),
             Dense(self.num_classes, activation='softmax')
         ])
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['Accuracy'])
-        
+        self.model.compile(
+            optimizer='adam', loss='categorical_crossentropy', metrics=['Accuracy'])
+
         # Convert labels to categorical
-        labels_categorical = to_categorical([self.label_to_index(label) for label in labels], num_classes=self.num_classes)
-        self.model.fit(data, labels_categorical, epochs=10, verbose=0)  # Train model quietly
+        self.labels_categorical = to_categorical([self.label_to_index(
+            label) for label in labels], num_classes=self.num_classes)
+
+    def train(self):
+        self.model.fit(self.data, self.labels_categorical,
+                       epochs=10, verbose=0)
+
+    def load(self, path):
+        self.model.load_weights(path)
 
     def label_to_index(self, label):
         return list(set(train_label)).index(label)
@@ -127,21 +152,26 @@ class FeedForwardNNModel(Model):
     def predict(self, sentence: str):
         # Convert the raw sentence to a bag-of-words feature vector and handle OOV
         sentence_bow = vectorizer.transform([sentence])
-        prediction = self.model.predict(sentence_bow.toarray(), verbose=0)[0]  # Suppress output here
+        prediction = self.model.predict(sentence_bow.toarray(), verbose=0)[
+            0]  # Suppress output here
         return self.index_to_label(np.argmax(prediction))
-
 
 
 if __name__ == "__main__":
     # Initialize the models with training data
-    mcm = MajorityClassModel(train_sentences, train_label)  # Majority Class Model
+    # Majority Class Model
+    mcm = MajorityClassModel(train_sentences, train_label)
     rbm = RuleBasedModel(train_sentences, train_label)  # Rule-Based Model
-    dtm = DecisionTreeModel(train_data_bow, train_label)  # Decision Tree Model
-    lrm = LogisticRegressionModel(train_data_bow, train_label)  # Logistic Regression Model
-    ffnn = FeedForwardNNModel(train_data_bow, train_label)  # Feed Forward NN Model
 
-    models = [mcm, rbm, dtm, lrm, ffnn]  # Add more models here so they get evaluated
+    dtm = DecisionTreeModel(train_data_bow, train_label)  # Decision Tree Model
+    # Logistic Regression Model
+    lrm = LogisticRegressionModel(train_data_bow, train_label)
+    # Feed Forward NN Model
+    ffnn = FeedForwardNNModel(train_data_bow, train_label)
+
+    # Add more models here so they get evaluated
+    models = [mcm, rbm, dtm, lrm, ffnn]
 
     # Example usage or testing on test data
     for model in models:
-        print(f"Example prediction from {model.name}: {model.predict(test_sentences[0])}")
+        print(f"Instantiated {model.name}")
