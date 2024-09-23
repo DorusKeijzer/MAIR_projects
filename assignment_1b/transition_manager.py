9f794ca9312b5092461eec8df5e800c687f00c6c
Dorus <doruskeijzer@gmail.com>
2024-09-23 16:31:50 +0200

    added conditions to transition manager

--- assignment_1b/transition_manager.py
+++ assignment_1b/transition_manager.py
@@ -2,23 +2,20 @@ class State:
     """Keeps track of one state and the states it transitions to. When initialzing states, it is easiest to start from the final state"""
 
     def __init__(self, name: str, transitions={}):
+        """A transition is a tuple consisting of 1. conditions 2. the state to transition to"""
         self.name = name
         self.transitions = transitions
 
     def __repr__(self) -> str:
         return self.name
 
-    def register_transition(self, dialogue_act: str, next_state: 'State'):
-        """Adds a new transition"""
-        print(f"Registering transition from {
-              self} to {next_state} when {dialogue_act}")
-        self.transitions[dialogue_act] = next_state
-
 
 class TransitionManager:
     """Manages the state transitions: takes a number of states and keeps track of the current state"""
 
     def __init__(self, states, starting_state=None):
+        self.preferences = {"food_type": None,
+                            "price_range":  None, "location": None}
         if states == []:
             raise Exception("States cannot be empty")
         self.states = states
@@ -34,16 +31,30 @@ class TransitionManager:
             raise Exception("Not a valid state")
         self.current_state = state
 
-    def register_state(self, state: State):
-        """Adds a new state"""
-        self.states.append(state)
+    def update_preferences(self, key: str, value: str):
+        if key not in self.preferences.keys():
+            raise KeyError("Key must be food_type, price_range, location")
+
+        self.preferences.key = value
 
     def transition(self, dialogue_act):
-        """Transitions to the next state based on the current dialogue act"""
+        """If conditions are met, transitions to the next state based on the current dialogue act"""
         if dialogue_act not in self.current_state.transitions.keys():
-            # defaults to other in case there is no transition specified for this dialogue act
-            dialogue_act = "other"
-        self.set_state(self.current_state.transitions[dialogue_act])
+            dialogue_act = "unknown"
+        new_state, conditions = self.current_state.transitions[dialogue_act]
+
+        if self._conditions_met(conditions):
+
+            self.set_state(self.current_state.transitions[dialogue_act])
+            return True
+        return False
+
+    def _conditions_met(conditions):
+        """Returns true if all conditions are met, else shortcuts to false"""
+        for condition in conditions:
+            if self.preferences[condition] == None:
+                return False
+        return True
 
     def __repr__(self):
         if self.current_state is None:
