bc912b57cc26b39ffd09939c06f1210341f6770a
Dorus <doruskeijzer@gmail.com>
2024-09-26 09:46:33 +0200

    added configuration file

---
+++ assignment_1c/config.py
@@ -0,0 +1,7 @@
+# this file can be used to configure the application,
+# simply set variables to true or false and import into main.py
+# the current content are just examples that are not implemented yet
+
+levenshtein = True
+all_caps = False
+...
